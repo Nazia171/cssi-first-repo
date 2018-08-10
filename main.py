924a064621859cf431c9dbbac9da738d1dc33342
Hassan Amjad <haxxter1977@gmail.com>
2018-08-09 20:41:09 -0400

    added to main.py & app.yaml

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #main.py
+import webapp2
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions =['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class HomePage(webapp2.RequestHandler):
+    def get(self):
+        home_template = the_jinja_env.get_template('templates/index.html')
+        self.response.write(home_template.render())
+
+
+app= webapp2.WSGIApplication([
+    ('/', HomePage)
+], debug=True)
