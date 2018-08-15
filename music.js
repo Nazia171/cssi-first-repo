4ad93fc6711fe448b7ba30be0776b53f6c5ad8bd
Hassan Amjad <haxxter1977@gmail.com>
2018-08-15 11:51:44 -0400

    Added files

--- music.js
+++ music.js
@@ -1,28 +1,366 @@
-// let box;
-// box = document.getElementById("overlay");
-// img = document.getElementByClassName("image");
-// function on() {
-//     // box.style.display = "block";
-//     // img.style.opacity = "0.9"
-//     $('#overlay').addClass('opClass');
-//     $('#image').addClass('opClass');
-//     $('#quotebox').addClass('opClass');
-//     $('#toptext').addClass('opClass');
-// }
-//
-// function off() {
-//     box.style.display = "none";
-// }
-// let image = document.querySelector('#image');
-// let home = document.querySelector('#home');
-// home.addEventListener('click', () => {
-//   image.style.filter = brightness("100%") ;
-//
-// });
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
+//h1
+$(function(){
+  var widget1 = SC.Widget("h1");
+    $("#playh1").click(function() {
+        widget1.play();
 
-function off() {
-    document.getElementById("overlay").style.display = "none";
-}
+    });
+
+    $("#pauseh1").click(function() {
+        widget1.pause()
+    });
+});
+//h2
+$(function(){
+  var widget1 = SC.Widget("h2");
+    $("#playh2").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauseh2").click(function() {
+        widget1.pause()
+    });
+});
+//h3
+$(function(){
+  var widget1 = SC.Widget("h3");
+    $("#playh3").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauseh3").click(function() {
+        widget1.pause()
+    });
+});
+//h4
+$(function(){
+  var widget1 = SC.Widget("h4");
+    $("#playh4").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauseh4").click(function() {
+        widget1.pause()
+    });
+});
+//h5
+$(function(){
+  var widget1 = SC.Widget("h5");
+    $("#playh5").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauseh5").click(function() {
+        widget1.pause()
+    });
+});
+///////////////////////////
+//s1
+$(function(){
+  var widget1 = SC.Widget("s1");
+    $("#plays1").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauses1").click(function() {
+        widget1.pause()
+    });
+});
+//s2
+$(function(){
+  var widget1 = SC.Widget("s2");
+    $("#plays2").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauses2").click(function() {
+        widget1.pause()
+    });
+});
+//s3
+$(function(){
+  var widget1 = SC.Widget("s3");
+    $("#plays3").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauses3").click(function() {
+        widget1.pause()
+    });
+});
+//s4
+$(function(){
+  var widget1 = SC.Widget("s4");
+    $("#plays4").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauses4").click(function() {
+        widget1.pause()
+    });
+});
+//s5
+$(function(){
+  var widget1 = SC.Widget("s5");
+    $("#plays5").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauses5").click(function() {
+        widget1.pause()
+    });
+});
+////////////
+//p1
+$(function(){
+  var widget1 = SC.Widget("p1");
+    $("#playp1").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausep1").click(function() {
+        widget1.pause()
+    });
+});
+//p2
+$(function(){
+  var widget1 = SC.Widget("p2");
+    $("#playp2").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausep2").click(function() {
+        widget1.pause()
+    });
+});
+//p3
+$(function(){
+  var widget1 = SC.Widget("p3");
+    $("#playp3").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausep3").click(function() {
+        widget1.pause()
+    });
+});
+//p4
+$(function(){
+  var widget1 = SC.Widget("p4");
+    $("#playp4").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausep4").click(function() {
+        widget1.pause()
+    });
+});
+//p5
+$(function(){
+  var widget1 = SC.Widget("p5");
+    $("#playp5").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausep5").click(function() {
+        widget1.pause()
+    });
+});
+///////////////////
+//r1
+$(function(){
+  var widget1 = SC.Widget("r1");
+    $("#playr1").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauser1").click(function() {
+        widget1.pause()
+    });
+});
+//r2
+$(function(){
+  var widget1 = SC.Widget("r2");
+    $("#playr2").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauser2").click(function() {
+        widget1.pause()
+    });
+});
+//r3
+$(function(){
+  var widget1 = SC.Widget("r3");
+    $("#playr3").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauser3").click(function() {
+        widget1.pause()
+    });
+});
+//r4
+$(function(){
+  var widget1 = SC.Widget("playerr4");
+    $("#playr4").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauser4").click(function() {
+        widget1.pause()
+    });
+});
+//r5
+$(function(){
+  var widget1 = SC.Widget("r5");
+    $("#playr5").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pauser5").click(function() {
+        widget1.pause()
+    });
+});
+///////////////
+//l1
+$(function(){
+  var widget1 = SC.Widget("l1");
+    $("#playl1").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausel1").click(function() {
+        widget1.pause()
+    });
+});
+//l2
+$(function(){
+  var widget1 = SC.Widget("l2");
+    $("#playl2").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausel2").click(function() {
+        widget1.pause()
+    });
+});
+//l3
+$(function(){
+  var widget1 = SC.Widget("l3");
+    $("#playl3").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausel3").click(function() {
+        widget1.pause()
+    });
+});
+//l4
+$(function(){
+  var widget1 = SC.Widget("l4");
+    $("#playl4").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausel4").click(function() {
+        widget1.pause()
+    });
+});
+//l5
+$(function(){
+  var widget1 = SC.Widget("l5");
+    $("#playl5").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausel5").click(function() {
+        widget1.pause()
+    });
+});
+/////////////
+//w1
+$(function(){
+  var widget1 = SC.Widget("w1");
+    $("#playw1").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausew1").click(function() {
+        widget1.pause()
+    });
+});
+//w2
+$(function(){
+  var widget1 = SC.Widget("w2");
+    $("#playw2").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausew2").click(function() {
+        widget1.pause()
+    });
+});
+//w3
+$(function(){
+  var widget1 = SC.Widget("w3");
+    $("#playw3").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausew3").click(function() {
+        widget1.pause()
+    });
+});
+//w4
+$(function(){
+  var widget1 = SC.Widget("w4");
+    $("#playw4").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausew4").click(function() {
+        widget1.pause()
+    });
+});
+//w5
+$(function(){
+  var widget1 = SC.Widget("w5");
+    $("#playw5").click(function() {
+        widget1.play();
+
+    });
+
+    $("#pausew5").click(function() {
+        widget1.pause()
+    });
+});
+///////////////////////////////////////
